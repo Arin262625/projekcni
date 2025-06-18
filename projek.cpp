2788e92fd6474c0137047e21729b306278144c6c
Karin262625 <cnurizza2gmail.com>
2025-06-18 12:49:14 +0800

    langkah keempat

--- projek.cpp
+++ projek.cpp
@@ -47,3 +47,57 @@ void menampilkanDataPelanggan() {
         cout << "- " << p.nama << " (Top Up: Rp " << p.topUp << ")\n";
     getch();
 }
+void jumlahData() {
+    system("cls");
+    int totalStok = 0, totalTopup = 0;
+    for (auto& i : inventory) {
+        totalStok += i.stok;
+    }
+    for (auto& p : pelanggan) {
+        totalTopup += p.topUp;
+    }
+
+cout << "Total stok seluruh item: " << totalStok << endl;
+    cout << "Total nilai top up pelanggan: Rp " << totalTopup << endl;
+    getch();
+}
+void dMenu() {
+    system("cls");
+    cout << "=== Aplikasi Inventory & Pelanggan Top Up ===\n";
+    cout << "1. Inisialisasi Data\n";
+    cout << "2. Tampilkan Inventory\n";
+    cout << "3. Tampilkan Data Pelanggan\n";
+    cout << "4. Total Semua Data\n";
+    cout << "5. Exit\n";
+    cout << "Masukkan angka: ";
+}
+int main() {
+    char pilih;
+    do {
+        dMenu();
+        pilih = getch();
+        switch (pilih) {
+            case '1': inisialisasiData(); 
+            break;
+
+            case '2': menampilkanInventory(); 
+            break;
+
+            case '3': menampilkanDataPelanggan(); 
+            break;
+
+            case '4': jumlahData(); 
+            break;
+            case '5': 
+            break;
+            
+             default:
+                system("cls");
+                cout << "Pilihan tidak tersedia!";
+                getch();
+                break;
+        }
+ } while (pilih != '8');
+
+  return 0;
+}
